0e486679cafde86f0d55915afb2e7e4d2e02db18
Ivan Hadzhiev <ivan.hadzhiev@news.co.uk>
2022-08-02 14:35:50 +0300

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,19 @@
 import React from 'react'
-import { useIntl } from 'react-intl'
-import logo from 'logo.svg'
+import {
+  BrowserRouter,
+  Routes as ReactRoutes,
+  Route
+} from 'react-router-dom'
+import { Routes } from 'routes/constants'
+import Index from 'routes/Index'
 import 'App.css'
 
-function App() {
-  const { formatMessage } = useIntl()
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-        <h1>{formatMessage({ id: 'component.home.title' })}</h1>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <BrowserRouter>
+    <ReactRoutes>
+      <Route path={Routes.Index} element={<Index />}/>
+    </ReactRoutes>
+  </BrowserRouter>
+)
 
 export default App
