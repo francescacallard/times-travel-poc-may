6f49af0736635034b44322b04906234877971a03
Ivan Hadzhiev <ivan.hadzhiev@news.co.uk>
2022-08-02 11:28:51 +0300

    Add basic sagas

--- src/redux/sagas/index.ts
+++ src/redux/sagas/index.ts
@@ -1,15 +1,8 @@
 import { all } from 'redux-saga/effects'
-import { common } from 'redux/sagas/common'
 import { errors } from 'redux/sagas/errors'
-import { articleIndex, articleReferrer } from 'redux/sagas/article'
-import { engagement } from 'redux/sagas/engagement'
 
 export default function* rootSaga () {
   yield all([
-    common(),
-    errors(),
-    articleIndex(),
-    articleReferrer(),
-    engagement()
+    errors()
   ])
 }
