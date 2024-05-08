433abb1030bd3c84d263c58fb298a8ceb90b1fa0
francescacallard <francescalily@outlook.com>
2024-05-08 16:52:29 +0100

    adding destinations

--- src/components/DestinationCards/index.jsx
+++ src/components/DestinationCards/index.jsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import './styles.css'
-import greece from '../../assets/greece.png'
+import React from 'react';
+import './styles.css';
 
-export const DestinationCards = () => {
+export const DestinationCards = ({ continent, country, image }) => {
   return (
-   <div className='destinationCardContainer'>
-    <h3 className='destinationCardContinent'>Europe</h3>
-    <h2 className='destinationCardCountry'>Italy</h2>
-    <div className='destinationImageContainer'>
-        <img src={greece}></img>
+    <div className='destinationCardContainer'>
+      <h3 className='destinationCardContinent'>{continent}</h3>
+      <h2 className='destinationCardCountry'>{country}</h2>
+      <div className='destinationImageContainer'>
+        <img src={image} alt={country} />
+      </div>
+      <button className='destinationSelectButton'>Select</button>
     </div>
-    <button className='destinationSelectButton'>Select</button>
-    </div>
-  )
-}
-
+  );
+};
--- src/components/Destinations/index.jsx
+++ src/components/Destinations/index.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
-import './styles.css'
-import { DestinationCards } from 'components/DestinationCards'
+import React from 'react';
+import './styles.css';
+import { DestinationCards } from 'components/DestinationCards';
+import greece from '../../assets/greece.png';
+
+const destinations = [
+  { continent: 'Europe', country: 'Italy', image: greece },
+  { continent: 'Asia', country: 'Japan', image: greece },
+  { continent: 'North America', country: 'USA', image: greece },
+  { continent: 'South America', country: 'Brazil', image: greece },
+  { continent: 'Africa', country: 'South Africa', image: greece },
+];
 
 export const Destinations = () => {
   return (
     <div className='destinationContainer'>
-        <h2 className='destinationHeading'>Great! Here are some amazing destinations for a relaxing trip in May</h2>
-        <h3 className='destinationSubHeading'>Select any of the below destinations that are of interest to you to refine your trip, you can save your progress for later!</h3>
-        <div className='destinationSuggestionContainer'>
-           <DestinationCards />
-        </div>
+      <h2 className='destinationHeading'>Great! Here are some amazing destinations for a relaxing trip in May</h2>
+      <h3 className='destinationSubHeading'>Select any of the below destinations that are of interest to you to refine your trip, you can save your progress for later!</h3>
+      <div className='destinationSuggestionContainer'>
+        {destinations.map((destination, index) => (
+          <DestinationCards
+            key={index}
+            continent={destination.continent}
+            country={destination.country}
+            image={destination.image}
+          />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
