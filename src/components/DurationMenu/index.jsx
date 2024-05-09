41007cbd58c937a1348128e77ab23f363322559c
francescacallard <francescalily@outlook.com>
2024-05-09 16:08:51 +0100

    adding duration color

--- src/components/DurationMenu/index.jsx
+++ src/components/DurationMenu/index.jsx
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 
 export const DurationMenu = ({ onDurationChange }) => {
+  const [selectedDuration, setSelectedDuration] = useState(null);
+
   const handleDurationClick = (duration) => {
+    setSelectedDuration(duration);
     onDurationChange(duration);
   };
 
   return (
     <div className='buttonContainer'>
-      <button className='button' onClick={() => handleDurationClick('4 - 5 days')}>
+      <button
+        className={`button ${selectedDuration === '4 - 5 days' ? 'active' : ''}`}
+        onClick={() => handleDurationClick('4 - 5 days')}
+      >
         4 - 5 days
       </button>
-      <button className='button' onClick={() => handleDurationClick('7 days')}>
+      <button
+        className={`button ${selectedDuration === '7 days' ? 'active' : ''}`}
+        onClick={() => handleDurationClick('7 days')}
+      >
         7 days
       </button>
-      <button className='button' onClick={() => handleDurationClick('14 days')}>
+      <button
+        className={`button ${selectedDuration === '14 days' ? 'active' : ''}`}
+        onClick={() => handleDurationClick('14 days')}
+      >
         14 days
       </button>
-      <button className='button' onClick={() => handleDurationClick('More')}>
+      <button
+        className={`button ${selectedDuration === 'More' ? 'active' : ''}`}
+        onClick={() => handleDurationClick('More')}
+      >
         More
       </button>
     </div>
