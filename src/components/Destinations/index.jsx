b3d9e80b1ef78e487edfb9b3199218dc4900ff7f
francescacallard <francescalily@outlook.com>
2024-05-09 10:50:40 +0100

    adding journalist recommendations

--- src/components/Destinations/index.jsx
+++ src/components/Destinations/index.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import './styles.css';
 import { DestinationCards } from 'components/DestinationCards';
+import { JournalistCard } from 'components/JournalistCard';
 import greece from '../../assets/greece.png';
 import france from '../../assets/france.png';
 import italy from '../../assets/italy.png';
+import timesFavicon from '../../assets/timesFavicon.svg';
+import { destinations, journalists } from './constants'
 
-const destinations = [
-  { continent: 'Europe', country: 'Italy', image: italy },
-  { continent: 'Europe', country: 'Greece', image: greece },
-  { continent: 'Europe', country: 'France', image: france },
-  { continent: 'South America', country: 'Brazil', image: greece },
-  { continent: 'Africa', country: 'South Africa', image: greece },
-];
+
 
 export const Destinations = () => {
   return (
+    <>
     <div className='destinationContainer'>
       <h2 className='destinationHeading'>Great! Here are some amazing destinations for a relaxing trip in May</h2>
       <h3 className='destinationSubHeading'>Select any of the below destinations that are of interest to you to refine your trip, you can save your progress for later!</h3>
@@ -29,5 +27,20 @@ export const Destinations = () => {
         ))}
       </div>
     </div>
+    <div className='suggestionMessage'>
+    <img src={timesFavicon} className='timesFavicon' alt='The Times Favicon' style={{ width: '24px', height: '24px' }}/>
+    <h3 className='suggestionHeading'>Suggestions created through sources from The Times and Sunday Times Journalists</h3>
+    </div>
+    <div className='journalistCardContainer'>
+        {journalists.map((journalist, index) => (
+          <JournalistCard
+            key={index}
+            name={journalist.name}
+            title={journalist.title}
+            image={journalist.image}
+          />
+        ))}
+      </div>
+   </>
   );
 };
---
+++ src/components/GenerateButton/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const GenerateButton = () => {
+  return (
+    <div>GenerateButton</div>
+  )
+}
+
