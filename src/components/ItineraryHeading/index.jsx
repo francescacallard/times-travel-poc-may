b90c599221fb42e9eea1b2afefa281bc0ba096e3
francescacallard <francescalily@outlook.com>
2024-05-13 15:08:17 +0100

    starting the two component cards

--- src/components/ItineraryHeading/index.jsx
+++ src/components/ItineraryHeading/index.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import './styles.css';
+import { ItineraryRecommendations } from 'components/ItineraryRecommendations';
 
 export const ItineraryHeading = ({ selectedHolidayType }) => {
   return (
-    <div className='ItineraryHeadingContainer'>
-      <h3 className='ItineraryHeadingText'>{`AI Response for ${selectedHolidayType}`}</h3>
-      <p className='ItineraryHeadingDescription'>
+    <div className='itineraryHeadingContainer'>
+      <h3 className='itineraryHeadingText'>{`AI Response for ${selectedHolidayType}`}</h3>
+      <p className='itineraryHeadingDescription'>
         This is a description of the AI response where it will talk about the itineraries of the trip chosen
       </p>
+      <div className='itineraryRecommendationsSide'>
+      <ItineraryRecommendations />
+      <ItineraryRecommendations />
+     </div>
     </div>
+
   );
 };
---
+++ src/components/ItineraryRecommendations/ItineraryDays/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.css'
+export const ItineraryDays = () => {
+  return (
+    <div className='itineraryDaysContainer'>
+      <h3 className='itineraryDayText'>Days</h3>
+      <h2 className='itineraryTitleText'>Title</h2>
+      <p className='itineraryDescriptionText'>Description</p>
+    </div>
+  )
+}
+
