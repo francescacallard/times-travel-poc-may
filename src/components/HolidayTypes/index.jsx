73a5c38c3a25b9dafa458ade639106dfc63b1537
francescacallard <francescalily@outlook.com>
2024-05-13 09:33:56 +0100

    morning commit

---
+++ src/components/HolidayTypes/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './styles.css'
+
+export const HolidayTypes = () => {
+  return (
+    <div className='textImageContainer'>
+      <img></img>
+      <div className='holidayTypesTextContainer'>
+        <h3 className='countryHeading'></h3>
+        <h2 className='holidayTypesHeading'></h2>
+        <p className='holidayTypesHeading'></p>
+        <button className='holidayTypesButton'>Select</button>
+      </div>
+    </div>
+  )
+}
