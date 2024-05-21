44fee3478ded2b2f0e628b2dbdacf5a9deb9097d
francescacallard <francescalily@outlook.com>
2024-05-21 10:42:20 +0100

    fixing padding

--- src/components/CallBack/index.jsx
+++ src/components/CallBack/index.jsx
@@ -14,7 +14,7 @@ export const CallBack = ({itinerary}) => {
       <h2 className='callBackHeading'>It’s much easier to tailor your trip to your needs over the phone, from flights, to hotels to experiences, we can ensure you have the perfect trip</h2>
       <p className='callBackSubheading'>You can call us on the number below or request a call back!</p>
       <div className='callBackButtonContainer'>
-        <button className='callBackButton'>
+        <button className='callBackButton' onClick={handleClick}>
           <img src={callA}></img> Request a Call Back
         </button>
         <button className='callBackButton' onClick={handleClick}>
